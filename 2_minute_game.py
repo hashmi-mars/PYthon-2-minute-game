3ff35c7514ae2bc4bd832055a08a1f73ef7aa70a
AbuBakar <hashmi.mars@gmail.com>
2022-02-11 14:46:46 +0500

    quick_Update_11feb2022
    Game is working fine

--- 2_minute_game.py
+++ 2_minute_game.py
@@ -1,9 +1,10 @@
-# PYthon-2-minute-game
-'''just a small 2 minute game - where I am teaching my students to make Python Conditions and If statements
+
+'''just a small 2 minute game -
+where I am teaching my students to make Python
+Conditions and If statements
 '''
 print("Let's play a game")
 print("You choose a number and this program would tell you the colour hiding under your number")
-
 print("You should choose from 1 to 4, otherwis it would give an error")
 
 a =  int(input("Please choose a number between 1 and 4"))
@@ -17,4 +18,4 @@ elif a == 3:
 elif a == 4:
     print("Yellow")
 else:
-    print("Error, you didn't choose right option")
+    print("Error you didn't choose right option...")
